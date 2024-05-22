1afb856e6c5fadfa71cbcb0cb2a04184f543d894
Leslie2186 <leslie.moraud@gmail.com>
2024-05-22 10:34:05 +0200

    Initialisation projet Cypress challenge 3

---
+++ cypress/e2e/challenge3.cy.js
@@ -0,0 +1,9 @@
+describe('CRUD complet sur API notes', () => {
+  it("Test d'accès sur l'api notes", () => {
+    cy.visit('https://practice.expandtesting.com/notes/api/api-docs/#/Notes/post_notes')
+  })
+
+  it("Test d'accès sur l'api notes", () => {
+    cy.visit('https://practice.expandtesting.com/notes/api/api-docs/#/Notes/post_notes')
+  })
+})
